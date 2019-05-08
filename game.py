3702754ede767f3e03d0329d5370cab2c868a4e5
Tom Robinson <tom@tjrobinson.net>
2019-05-08 20:24:06 +0100

    First commit

---
+++ game.py
@@ -0,0 +1,10 @@
+scoredaddy = 0
+scoreleo = 0
+print("create your character leo!")
+nameleo = "leo"
+ageleo = input("How old is your character leo?")
+strengthsleo = input("What are your characters strengths leo?")
+weaknesses = input("what are your characters weaknesses?")
+print("leo your characters weaknesses " + weaknesses)
+print("leo your strengths are" + strengthsleo)
+print("leo your name is" + nameleo)
