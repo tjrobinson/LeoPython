a5e0f5ea5614d2f51ca1fefe4e718ba6ab668610
Tom Robinson <tom@tjrobinson.net>
2019-06-15 21:33:20 +0100

    Add .gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='LeoPython',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Leo Robinson',
+    author_email='',
+    description=''
+)
