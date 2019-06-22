8267dbe75c0045b65cfbd9e0ba8461710d5afdd5
Leo Robinson <leojohnrobinson@gmail.com>
2019-06-22 20:58:07 +0100

    Latest changes

--- my-game.py
+++ my-game.py
@@ -1,3 +1,4 @@
+numbers = ["1","2","3","4","5","6","7","69","82"]
 score = 0
 playername = input("choose a name")
 friendsname = "zazwaddldoodle"
@@ -6,21 +7,27 @@ print("Yes.")
 print("Really?")
 print("I said 'yes'"+ friendsname+"!")
 print("Alright!")
-answer1 = "sixty nine"
-playerchoice1 = input("what is twenty three times three?")
+answer1 = numbers.8
+def .number(numbers):
+    playerchoice1 = input("what is twenty three times three?")
+    playerchoice1 = playerchoice1.number
+    if playerchoice1 == answer1:
+        score = 49
 if playerchoice1 != answer1:
     print("incorrect,")
     print("try again.")
     print("The answer was"+str(answer1))
+    if score > 50
+        score = score - 50
 if playerchoice1 == answer1:
     print("corect")
     print("score + 1")
     scorearned = 1
     score = score + scorearned
-answer2 = "seventy two"
+answer2 = numbers.9
 print("your score is "+str(score))
 print(playername)
-playerchoice2 = input("what is eight times nine(answer in words)?")
+playerchoice2 = input("what is eight times nine?")
 if playerchoice2 != answer2:
     print("incorrect,")
     print("try again.")
